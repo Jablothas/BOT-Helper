9f78372886b7e952e4ef09513f3850ed3d8b3282
Kevin Wong <kevin.wong@web.de>
2024-03-19 06:17:54 +0100

    weitere kommentare

--- running-bunny/RaumZeitPlan/RaumZeitPlanErstellung.cs
+++ running-bunny/RaumZeitPlan/RaumZeitPlanErstellung.cs
@@ -41,20 +41,20 @@ namespace running_bunny.RaumZeitPlan
         private void WunschProVeranstaltung()
         {
             int counter = 0;
-            foreach(Veranstaltung veranstaltung in VeranstaltungsListe)
+            foreach(Veranstaltung veranstaltung in VeranstaltungsListe) //Durchsucht alle Veranstaltungen
             {
                 counter++;
-                foreach(Schueler schueler in SchuelerListe)
+                foreach(Schueler schueler in SchuelerListe) //Durchsucht alle Schüler
                 {
-                    Wunsch wunsch = schueler.Wuensche.Find(swu => swu.VeranstaltungsId == veranstaltung.Id && swu.Prioritaet < 6);
-                    if (wunsch != null)
+                    Wunsch wunsch = schueler.Wuensche.Find(swu => swu.VeranstaltungsId == veranstaltung.Id && swu.Prioritaet < 6); //Sucht ersten Wunsch mit gleicher VeranstaltungsID und einer Priorität < 6
+                    if (wunsch != null) //Falls kein wunsch gefunden wurde, wird Wunsch übersprungen
                     {
-                        veranstaltung.AnzahlWünsche = veranstaltung.AnzahlWünsche + 1;
+                        veranstaltung.AnzahlWünsche = veranstaltung.AnzahlWünsche + 1; //Erhöht die Anzahl der Wünsche in Veranstaltung um 1
                     }
                     //veranstaltung.AnzahlWünsche++; //wird setter aufgerufen?
                     
                 }
-                veranstaltung.BerechneBenoetigteRaeume();
+                veranstaltung.BerechneBenoetigteRaeume(); 
                 Debug.WriteLine($"{counter}.     " + veranstaltung.UnternehmensName + " " + veranstaltung.Fachrichtung + " " + veranstaltung.AnzahlWünsche + " Benötigte Räume " + veranstaltung.AnzahlRaeume + "\n");
             }
             Debug.WriteLine("///////////////////////////"); Debug.WriteLine("///////////////////////////");
@@ -125,23 +125,23 @@ namespace running_bunny.RaumZeitPlan
         public void RaumZeitPlanZeilenweiseZuweisung()
         {
             MischeRaumListe();
-            foreach(Raum raum in RaumListe)
+            foreach(Raum raum in RaumListe) //Durchsucht Raum in Raumliste
             {
-                if(!raum.IstRaumVoll())
+                if(!raum.IstRaumVoll()) //Fragt ab, ob Raum voll ist
                 {
-                    foreach (Veranstaltung ver in VeranstaltungsListe)
+                    foreach (Veranstaltung ver in VeranstaltungsListe) //Durchsucht Veranstaltungen in Veranstaltungsliste
                     {
-                        if(ver.RaeumeBesetzt < ver.AnzahlRaeume && !raum.IstRaumVoll())
+                        if(ver.RaeumeBesetzt < ver.AnzahlRaeume && !raum.IstRaumVoll()) //Fragt ob, die Anzahl der reservierten Räume kleiner als die Anahl der benötigten Räumen in Veranstaltung sind
                         {
-                            int index = FindeFreieStelle(ver, raum);
-                            if (index != -1)
+                            int index = FindeFreieStelle(ver, raum); //Übergibt Veranstaltung und Raum um eine freie Stelle zu suchen
+                            if (index != -1) //Bei -1 keine Stelle gefunden
                             {
                                 for (int i = 0; i < ver.AnzahlRaeume; i++)
                                 {
-                                    ver.RaeumeBesetzt++;
-                                    ZelleRaumZeitplan zelle = new ZelleRaumZeitplan((Zeitslot)(index + i + 1), ver, raum);
-                                    raum.BelegteSlots[index + i] = zelle;
-                                    RaumZeitplan.Add(zelle);
+                                    ver.RaeumeBesetzt++; //erhöhung anzahl besesetzte räume um 1
+                                    ZelleRaumZeitplan zelle = new ZelleRaumZeitplan((Zeitslot)(index + i + 1), ver, raum); //zelle wird erstellt
+                                    raum.BelegteSlots[index + i] = zelle;  //raumbelegung wird abgespeichert
+                                    RaumZeitplan.Add(zelle); //zelle wird in RaumZeitPlanliste hinzugefügt
 
                                 }
                             }
@@ -168,26 +168,26 @@ namespace running_bunny.RaumZeitPlan
 
         public int FindeFreieStelle(Veranstaltung ver, Raum raum)
         {
-            int benoetigteRaueme = ver.AnzahlRaeume;
+            int benoetigteRaueme = ver.AnzahlRaeume; //Speichert Anzahl Räume ab
             int index = -1; // -1 => keine freie Stelle gefunden
-            for(int i = 0; i < raum.BelegteSlots.Length; i++)
+            for(int i = 0; i < raum.BelegteSlots.Length; i++) //iteriert durch alle Slots
             {
-                for (int k = 0; k < benoetigteRaueme; k++)
+                for (int k = 0; k < benoetigteRaueme; k++) //iteriert durch benötigte Räume
                 {
-                    int indexMomentan = i + k;
+                    int indexMomentan = i + k; //Fasst i und k zusammmen um aktuellen index zu speichern
 
-                    if (indexMomentan <= raum.BelegteSlots.Length - 1 && raum.BelegteSlots[indexMomentan] == null && indexMomentan >= (int)ver.FruehsterZeitSlot - 1)
+                    if (indexMomentan <= raum.BelegteSlots.Length - 1 && raum.BelegteSlots[indexMomentan] == null && indexMomentan >= (int)ver.FruehsterZeitSlot - 1) //Fragt ob, index gültigkeit hat
                     {
-                        if (k == benoetigteRaueme - 1) return index = i;
+                        if (k == benoetigteRaueme - 1) return index = i; //falls das ende erreicht wurde, gibt index zurück
                         continue;
                     }
                     else
-                        break;
+                        break; //Falls Bedingungen nicht gelten, wird innere schleife abgebrochen
                 }
             }
             return index;
         }
-        private void MischeRaumListe()
+        private void MischeRaumListe() //Durchmischt räume in Raumliste 
         {
             //mischt die Raumliste vorab
             if (istgemischt == false)
